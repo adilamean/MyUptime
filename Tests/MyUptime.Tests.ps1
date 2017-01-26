beb447d332a2421fe18ea368227fd60543bc9ac5
Adil Rathore <adilrathore@gmail.com>
2017-01-27 04:30:29 +0530

    Auto Update Module lines added

--- Tests/MyUptime.Tests.ps1
+++ Tests/MyUptime.Tests.ps1
@@ -2,9 +2,16 @@
 
 #Pester tests for MyUptime
 
+
+$projectRoot = Resolve-Path "$PSScriptRoot\.."
+$moduleRoot = Split-Path (Resolve-Path "$projectRoot\*\*.psd1")
+$moduleName = Split-Path $moduleRoot -Leaf
+
+Import-Module (Join-Path $moduleRoot "$moduleName.psm1") -force
+
 #Import-Module MyUptime
 
-InModuleScope MyUptime -Tags Build{
+InModuleScope MyUptime {
 Describe "Get-MyUptime" {
     
     It "Should run without error by default" {
